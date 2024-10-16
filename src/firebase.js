c4025acc785c8bc63f957b6efcef113de49af7f4
Jiaqi <yjq0323@gmail.com>
2024-10-16 11:48:31 +0800

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -18,13 +18,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIXJ5YT7hoNbBFqK3TBcV41-TzIO-7n7w",
-  authDomain: "fir-auth-6edd8.firebaseapp.com",
-  projectId: "fir-auth-6edd8",
-  storageBucket: "fir-auth-6edd8.appspot.com",
-  messagingSenderId: "904760319835",
-  appId: "1:904760319835:web:44fd0d957f114b4e51447e",
-  measurementId: "G-Q4TYKH9GG7",
+  apiKey: "AIzaSyDVh7Pfw2A8nQ3xUzC0cNAevDCNJkkv7eM",
+  authDomain: "is4301-agile-90c04.firebaseapp.com",
+  projectId: "is4301-agile-90c04",
+  storageBucket: "is4301-agile-90c04.appspot.com",
+  messagingSenderId: "401429222812",
+  appId: "1:401429222812:web:5bf4806cf8ce5b8f4ba459",
+  measurementId: "G-16W6XNF534"
 };
 
 const app = initializeApp(firebaseConfig);
